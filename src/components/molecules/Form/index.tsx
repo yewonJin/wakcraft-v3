e62379c25707de27a4040d763defa815bae5cf8a
yewonJin <jyw966@naver.com>
2024-05-11 16:30:20 +0900

    [UPDATE] width, height 커스텀화 가능

--- src/components/molecules/Form/index.tsx
+++ src/components/molecules/Form/index.tsx
@@ -7,13 +7,32 @@ type InputFormProps = {
   name: string
   value: string | number
   handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  width?: string
+  height?: string
 }
 
-export const InputForm = ({ label, type = 'text', name, value, placeholder, handleInputChange }: InputFormProps) => {
+export const InputForm = ({
+  label,
+  type = 'text',
+  name,
+  value,
+  placeholder,
+  handleInputChange,
+  width,
+  height,
+}: InputFormProps) => {
   return (
     <div className="flex flex-col gap-2">
       <label className="text-text-secondary">{label}</label>
-      <Input type={type} name={name} value={value} placeholder={placeholder} handleInputChange={handleInputChange} />
+      <Input
+        type={type}
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        handleInputChange={handleInputChange}
+        width={width}
+        height={height}
+      />
     </div>
   )
 }
