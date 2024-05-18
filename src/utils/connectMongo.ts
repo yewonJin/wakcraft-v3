13484995d59894e345dd8253ea90f5193dc0ed73
yewonJin <jyw966@naver.com>
2024-05-19 00:32:06 +0900

    [UPDATE] 개발 환경에서 DB test용으로 설정

--- src/utils/connectMongo.ts
+++ src/utils/connectMongo.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectMongo = async () =>
   mongoose.connect(process.env.EC2_MONGO_URI as string, {
-    dbName: 'wakcraft',
+    dbName: process.env.NODE_ENV === 'production' ? 'wakcraft' : 'wakcraft-test',
   })
 
 export default connectMongo
