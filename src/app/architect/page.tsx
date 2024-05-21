fb01ef6b615a721d32fb2b5d205fddcd766a8ffe
yewonJin <jyw966@naver.com>
2024-05-21 19:48:20 +0900

    [UPDATE] 페이지 제목 서버 컴포넌트로 이동

--- src/app/architect/page.tsx
+++ src/app/architect/page.tsx
@@ -1,14 +1,19 @@
+import { Fragment, Suspense } from 'react'
+
 import ArchitectHome from '@/components/templates/ArchitectHome'
+import PageTitle from '@/components/organisms/Common/PageTitle'
 
 import { getAllArchitects } from '@/apis/server/architect'
-import { Suspense } from 'react'
 
 export default async function Page() {
   const architects = await getAllArchitects()
 
   return (
-    <Suspense>
-      <ArchitectHome architects={JSON.parse(JSON.stringify(architects))} />
-    </Suspense>
+    <Fragment>
+      <PageTitle title="건축가" summary="마인크래프트 눕프핵 건축가들의 포트폴리오를 볼 수 있다." />
+      <Suspense>
+        <ArchitectHome architects={JSON.parse(JSON.stringify(architects))} />
+      </Suspense>
+    </Fragment>
   )
 }
--- src/components/templates/ArchitectHome/index.tsx
+++ src/components/templates/ArchitectHome/index.tsx
@@ -2,7 +2,7 @@
 
 import ArchitectCategory from '@/components/organisms/Architect/ArchitectCategory'
 import ArchitectList from '@/components/organisms/Architect/ArchitectList'
-import PageTitle from '@/components/organisms/Common/PageTitle'
+
 import useFilteringByTier from '@/hooks/useFilteringByTier'
 import useSearch from '@/hooks/useSearch'
 import useSortArchitect from '@/hooks/useSortArchitect'
@@ -19,7 +19,6 @@ export default function ArchitectHome({ architects }: Props) {
 
   return (
     <main className="flex max-w-[1200px] flex-col mx-auto">
-      <PageTitle title="건축가" summary="마인크래프트 눕프핵 건축가들의 포트폴리오를 볼 수 있다." />
       <ArchitectCategory
         input={input}
         handleCategoryClick={handleCategoryClick}
