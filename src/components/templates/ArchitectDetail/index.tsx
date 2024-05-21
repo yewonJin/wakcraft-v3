e97b50b85e95920d90361f3ff632fbce9507a58a
yewonJin <jyw966@naver.com>
2024-05-21 16:10:08 +0900

    [UPDATE] queryString -> state로 정렬 방식 변경

--- src/components/templates/ArchitectDetail/index.tsx
+++ src/components/templates/ArchitectDetail/index.tsx
@@ -1,33 +1,32 @@
 'use client'
 
+import { useState } from 'react'
+
 import ArchitectInfo from '@/components/molecules/ArchitectInfo'
 import ArchitectPortfolioCategory from '@/components/organisms/Architect/ArchitectPortfolioCategory'
 import ArchitectPortfolioList from '@/components/organisms/Architect/ArchitectPortfolioList'
 
 import { Architect } from '@/types/architect'
-import { useQueryString } from '@/hooks/useQueryString'
 
 type Props = {
   architect: Architect
 }
 
+type Category = '' | 'noobprohacker' | 'placementTest' | 'eventNoobProHacker'
+
 export default function ArchitectDetail({ architect }: Props) {
-  const { queryString, resetQueryString, setQueryString } = useQueryString('category')
+  const [category, setCategory] = useState<Category>('')
 
   const handleCategoryClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-    if (e.currentTarget.name === '') {
-      return resetQueryString()
-    }
-
-    setQueryString(e.currentTarget.name)
+    setCategory(e.currentTarget.name as Category)
   }
 
   return (
     <main className="flex min-h-screen flex-col gap-8">
       <title>{'왁크래프트 | ' + architect.wakzoo_id}</title>
       <ArchitectInfo type="detail" architect={architect} />
-      <ArchitectPortfolioCategory category={queryString || ''} handleCategoryClick={handleCategoryClick} />
-      <ArchitectPortfolioList portfolio={architect.portfolio} category={queryString || ''} />
+      <ArchitectPortfolioCategory category={category} handleCategoryClick={handleCategoryClick} />
+      <ArchitectPortfolioList portfolio={architect.portfolio} category={category} />
     </main>
   )
 }
