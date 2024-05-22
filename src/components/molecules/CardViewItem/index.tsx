73ddf9d7dd95a00b742d4326e84495eae5c9203c
yewonJin <jyw966@naver.com>
2024-05-23 01:25:50 +0900

    [UPDATE] 컨텐츠 뱃지 추가 및 유튜브 링크 버튼 변경

--- src/components/molecules/CardViewItem/index.tsx
+++ src/components/molecules/CardViewItem/index.tsx
@@ -6,8 +6,8 @@ import Link from 'next/link'
 
 import Icon from '@/components/atoms/Icon'
 import Typography from '@/components/atoms/Typography'
-import { getDateString } from '@/utils/shared'
 import { Content } from '@/types/content'
+import { getDateString } from '@/utils/shared'
 
 type Props = {
   type: Content
@@ -15,15 +15,24 @@ type Props = {
   subject: string
   youtube_url: string
   linesSubject?: string[]
+  isContributedContent?: boolean
   date: Date
 }
 
-const getYoutubeThumbnailImageUrl = (youtube_url: string) => {
-  return `https://i.ytimg.com/vi/${youtube_url.split('/')[3]}/hq720.jpg`
-}
-
-export default function CardViewItem({ type, episode, subject, youtube_url, linesSubject, date }: Props) {
+export default function CardViewItem({
+  type,
+  episode,
+  subject,
+  youtube_url,
+  linesSubject,
+  date,
+  isContributedContent,
+}: Props) {
   const router = useRouter()
+
+  const getYoutubeThumbnailImageUrl = (youtube_url: string) => {
+    return `https://i.ytimg.com/vi/${youtube_url.split('/')[3]}/hq720.jpg`
+  }
 
   const getContentLinkUrl = (type: Content, episode: number) => {
     switch (type) {
@@ -69,6 +78,36 @@ export default function CardViewItem({ type, episode, subject, youtube_url, line
     }
   }
 
+  const generateContentBadge = () => {
+    if (isContributedContent) {
+      return (
+        <span className=" bg-lime-900 text-[#ddd] text-sm px-2 py-1 rounded-md top-2 left-2 absolute">조공 컨텐츠</span>
+      )
+    }
+
+    if (type === '이벤트 눕프핵') {
+      return (
+        <span className=" bg-rose-900  text-[#ddd] text-sm px-2 py-1 rounded-md top-2 left-2 absolute">
+          이벤트 눕프핵
+        </span>
+      )
+    }
+
+    if (type === '배치고사') {
+      return (
+        <span className=" bg-amber-900 text-[#ddd] text-sm px-2 py-1 rounded-md top-2 left-2 absolute">배치고사</span>
+      )
+    }
+
+    if (type === '건축 콘테스트') {
+      return (
+        <span className=" bg-amber-900 text-[#ddd] text-sm px-2 py-1 rounded-md top-2 left-2 absolute">
+          건축 콘테스트
+        </span>
+      )
+    }
+  }
+
   return (
     <Link href={getContentLinkUrl(type, episode)}>
       <div className="group h-min rounded-xl bg-background-secondary duration-300 hover:-translate-y-2 hover:cursor-pointer">
@@ -78,23 +117,13 @@ export default function CardViewItem({ type, episode, subject, youtube_url, line
           ) : (
             <div></div>
           )}
+          {generateContentBadge()}
         </div>
         <div className="px-4 pb-4 pt-6 [&>p]:text-end flex flex-col gap-4">
           <div className="flex items-center gap-3 ">
             <Typography variants="h2" style={{ fontSize: '20px', lineHeight: '24px' }}>
               {getContentTitle(type, subject, episode)}
             </Typography>
-            {youtube_url !== 'null' && (
-              <div
-                onClick={(e) => {
-                  e.preventDefault()
-                  window.open(youtube_url)
-                }}
-                className="[&>div>svg]:fill-text-secondary [&>div>svg]:rotate-[135deg] [&>div>svg]:hover:fill-text-primary"
-              >
-                <Icon type="link" width="24px" height="24px" pointerOnHover />
-              </div>
-            )}
           </div>
           {linesSubject && (
             <div className="flex flex-wrap gap-2 text-sm text-text-tertiary">
@@ -110,9 +139,25 @@ export default function CardViewItem({ type, episode, subject, youtube_url, line
               ))}
             </div>
           )}
-          <Typography variants="p" color="tertiary" style={{ fontSize: '14px' }}>
-            {getDateString(date)}
-          </Typography>
+          <div className="flex justify-between items-center border-t-[1px] pt-3 border-background-tertiary">
+            <div className="flex rounded-md items-center gap-1">
+              {youtube_url !== 'null' && (
+                <div
+                  className="flex items-center text-text-tertiary text-sm bg-background-primary px-2 py-1 gap-1 rounded-md [&>div>svg]:fill-text-tertiary hover:bg-background-tertiary"
+                  onClick={(e) => {
+                    e.preventDefault()
+                    window.open(youtube_url)
+                  }}
+                >
+                  <Icon type="link" width="20px" height="20px" />
+                  <span>유튜브</span>
+                </div>
+              )}
+            </div>
+            <Typography variants="p" color="tertiary" style={{ fontSize: '14px' }}>
+              {getDateString(date)}
+            </Typography>
+          </div>
         </div>
       </div>
     </Link>
