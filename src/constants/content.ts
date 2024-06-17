a53a6e86910d4d05043642c5423541821847462e
yewonJin <jyw966@naver.com>
2024-06-17 17:26:48 +0900

    [UPDATE] 무제한 시간 컨텐츠 추가

--- src/constants/content.ts
+++ src/constants/content.ts
@@ -1,5 +1,5 @@
 /** 무제한 시간이었던 이벤트 눕프로해커 회차 */
-export const INFINITE_TIME_EVENT_NOOBPROHACKER = [1, 4, 6, 9]
+export const INFINITE_TIME_EVENT_NOOBPROHACKER = [1, 4, 6, 9, 10]
 
 /** 무제한 시간이었던 이벤트 건축 콘테스트 회차 */
 export const INFINITE_TIME_ARCHITECTURE_CONTEST = [2, 3]
