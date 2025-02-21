a2da61e8d41c1475a18fa444bc1f869440d180f6
yewonJin <jyw966@naver.com>
2025-02-21 16:08:52 +0900

    [FIX] 빌드 오류 해결

--- src/app/architect/page.tsx
+++ src/app/architect/page.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from 'react'
+import { Fragment, Suspense } from 'react'
 
 import ArchitectHome from '@/components/templates/ArchitectHome'
 import PageTitle from '@/components/organisms/Common/PageTitle'
@@ -11,7 +11,9 @@ export default async function Page() {
   return (
     <Fragment>
       <PageTitle title="건축가" summary="마인크래프트 눕프핵 건축가들의 포트폴리오를 볼 수 있다." />
-      <ArchitectHome architects={JSON.parse(JSON.stringify(architects))} />
+      <Suspense>
+        <ArchitectHome architects={JSON.parse(JSON.stringify(architects))} />
+      </Suspense>
     </Fragment>
   )
 }
